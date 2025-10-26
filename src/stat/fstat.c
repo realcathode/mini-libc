9b13c6fa24b5adb57b6729403c60f4526acf18aa
realcathode <ion.miron@tutanota.com>
2025-10-26 17:01:45 +0200

    Implement `fstat` function

--- src/stat/fstat.c
+++ src/stat/fstat.c
@@ -6,6 +6,12 @@
 
 int fstat(int fd, struct stat *st)
 {
-	/* TODO: Implement fstat(). */
-	return -1;
+	long ret = syscall(__NR_fstat, fd, st);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+
+	return 0;
 }
